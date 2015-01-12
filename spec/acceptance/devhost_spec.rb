6754c2e6d42809086c6fa6fda36a6cfa7319ec11
mtbvang <mtb.vang@gmail.com>
2015-01-12 11:10:53 +0100

    Completed acceptance tests.

--- spec/acceptance/devhost_spec.rb
+++ spec/acceptance/devhost_spec.rb
@@ -9,7 +9,7 @@ describe 'devhost class' do
       EOS
 
       # Run it twice and test for idempotency
-      #expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
     end
 
@@ -21,8 +21,52 @@ describe 'devhost class' do
       it { should be_directory }
     end
 
+    describe file('/usr/bin/eclipse') do
+      it { should be_symlink }
+    end
+
+    describe file('/usr/lib/eclipse') do
+      it { should be_directory }
+    end
+
+    describe command('cat /usr/lib/eclipse/.eclipseproduct | grep version') do
+      its(:stdout) { should match(/version=4.3.2/) }
+    end
+
     describe command('vagrant -v') do
       its(:stdout) { should match(/Vagrant 1.6.3/) }
+    end
+
+    describe package('virtualbox-4.3') do
+      it { should be_installed }
+    end
+
+    describe command('docker version') do
+      its(:stdout) { should match(/Client version: 1.3.1/) }
+    end
+
+    describe package('sublime-text') do
+      it { should be_installed }
+    end
+
+    describe command('bundle version') do
+      its(:stdout) { should match(/Bundler version 1.3.5/) }
+    end
+
+    describe command('librarian-puppet version') do
+      its(:stdout) { should match(/librarian-puppet v2.0.1/) }
+    end
+
+    describe package('skype') do
+      it { should be_installed }
+    end
+
+    describe package('nautilus-dropbox') do
+      it { should be_installed }
+    end
+
+    describe command('phing --version') do
+      its(:stdout) { should match(/Phing 2.8.0/) }
     end
 
   end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -8,7 +8,7 @@ hosts.each do |host|
   install_puppet(:version => '3.6.2')
   install_package host, 'git'
   install_package host, 'bundler'
-  on host, 'gem install librarian-puppet'
+  on host, "gem install librarian-puppet -v 2.0.1 --source 'https://rubygems.org'"
   #install_librarian
 end
 
@@ -17,15 +17,16 @@ RSpec.configure do |c|
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
   # Readable test descriptions
-  c.formatter = :documentation
+  #c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
     # Install dependencies using librarian-puppet
     librarian_install_modules(proj_root, 'devhost')
     # Install module under test
-    puppet_module_install(:source => proj_root, :module_name => 'devhost')
+    #puppet_module_install(:source => proj_root, :module_name => 'devhost')
     hosts.each do |host|
+      on host, "echo project_root: #{proj_root}"
       on host, 'ls -la /etc/puppet/modules'
       on host, "echo #{host['distmoduledir']}"
       on host, 'puppet config print modulepath'
